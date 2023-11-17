1ea23d5382ff7562f27c12383b73390ff9c5f24e
Josh <134442015+Blixxky@users.noreply.github.com>
2023-11-17 11:08:17 -0500

    Update main.py
    Added natural mouse movements instead of instant click
    Added a countdown so user is aware of what bot is doing

--- main.py
+++ main.py
@@ -3,10 +3,18 @@ import random
 import pyautogui
 import cv2
 
+def countdown_timer(seconds):
+    for remaining in range(seconds, 0, -1):
+        print(f"Leaving battle in {remaining} seconds...")
+        time.sleep(1)
+
 def find_and_click(image_path, confidence=0.7, sleep_time=0):
     if sleep_time > 0:
-        print("Waiting...")
-        time.sleep(sleep_time)
+        print(f"Waiting for {image_path}...")
+        if image_path == image_sequence[3]:
+            countdown_timer(int(sleep_time))
+        else:
+            time.sleep(sleep_time)
     else:
         print(f"Looking for {image_path}...")
 
@@ -27,7 +35,10 @@ def find_and_click(image_path, confidence=0.7, sleep_time=0):
             with open("BOOM.LOG", "a") as log_file:
                 log_file.write(f"Unable to locate {image_path}. Retrying...\n")
 
-    pyautogui.click(location)
+    # Add natural mouse movement before clicking
+    pyautogui.moveTo(location, duration=random.uniform(0.1, 0.5))
+
+    pyautogui.click()
     print(f"Clicked on {image_path}")
 
 # Main loop
@@ -36,8 +47,8 @@ image_sequence = ["portrait.png", "play.png", "confirm.png", "settings.png", "le
 
 while True:
     find_and_click(image_sequence[0], confidence=0.7, sleep_time=3)
-    find_and_click(image_sequence[1], confidence=0.7, sleep_time=10)
-    find_and_click(image_sequence[2], confidence=0.7, sleep_time=5)
+    find_and_click(image_sequence[1], confidence=0.7, sleep_time=6)
+    find_and_click(image_sequence[2], confidence=0.7, sleep_time=15)
     find_and_click(image_sequence[3], confidence=0.7, sleep_time=random.uniform(1111, 1222))
     find_and_click(image_sequence[4], confidence=0.7, sleep_time=3)
     find_and_click(image_sequence[5], confidence=0.7, sleep_time=4)
